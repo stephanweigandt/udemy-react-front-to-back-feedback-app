82afe9db33ac85d4b3cc852d0360f2553e4a22f1
Stephan Weigandt <stephan.weigandt@yahoo.com>
2022-12-02 14:42:20 -0800

    initial

---
+++ src/App_easy_example.js
@@ -0,0 +1,37 @@
+function App() {
+    const title = 'Blog Post'
+    const body = 'This is my blog post'
+    const comments = [
+        {id: 1, text: 'Comment One'},
+        {id: 2, text: 'Comment Two'},
+        {id: 3, text: 'Comment Three'},
+        {id: 4, text: 'Comment Four'}
+    ]
+    const loading = false
+    const showComments = true
+    const commentBlock = (
+        <div className="comments">
+        <h3>Comments ({comments.length})</h3>
+        <ul>
+            {comments.map((comment, index)=>(
+                <li key={index}>{comment.text}</li>
+
+            ))}
+        </ul>
+    </div>            
+    )
+
+    if (loading) return <h1>Loading...</h1>
+
+    return (
+        <div className="container">
+            <h1>{title.toUpperCase()}</h1>
+            <p>{body}</p>
+
+            {showComments && commentBlock }
+
+
+        </div>
+    )
+}
+export default App
